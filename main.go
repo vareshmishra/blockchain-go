bd97fb7a5dbf0e198097ab7282ee78eb032a7bb3
Varesh Mishra <vareshmishra@gmail.com>
2021-02-15 21:32:18 +0530

    Refactor

--- main.go
+++ main.go
@@ -1,58 +1,24 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+
+	"github.com/vareshmishra/blockchain-go/blockchain"
 )
-
-type BlockChain struct {
-	blocks []*Block
-}
-
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-func (b *Block) CalcuateHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.CalcuateHash()
-	return block
-}
-
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis Block", []byte{})
-}
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
-}
 
 func main() {
 	fmt.Println("Testing Blockchain")
 
-	chain := InitBlockChain()
+	chain := blockchain.InitBlockChain()
 	chain.AddBlock("Second Block")
 	chain.AddBlock("Third Block")
 	chain.AddBlock("Fourth Block")
 
-	for _, blk := range chain.blocks {
+	for _, blk := range chain.Blocks {
 		fmt.Printf("Previous Hash: %x\n", blk.PrevHash)
 		fmt.Printf("Data in Block: %s\n", blk.Data)
 		fmt.Printf("Hash: %x\n", blk.Hash)
+		fmt.Println("================")
+
 	}
 }
